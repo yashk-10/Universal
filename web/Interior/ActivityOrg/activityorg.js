d4ff33aa7595146389180e2306a8262297f82026
VishnudevP <62526318+VishnudevP@users.noreply.github.com>
2020-11-08 01:10:44 -0500

    Final Update

--- web/Interior/ActivityOrg/activityorg.js
+++ web/Interior/ActivityOrg/activityorg.js
@@ -4,11 +4,52 @@ var security = document.getElementById('security');
 var custConf = document.getElementById('customerConf');
 var reqComplete = document.getElementById('reqComplete');
 var orgButton = document.getElementById('orgButton');
-var compCreComplete = document.getElementById('compCrecomplete');
-var reqButton = document.getElementById('reqButton');
+var compCreComplete = document.getElementById('compCreComplete');
+var reqButton = document.getElementById('reqbutton');
+var requestMade = document.getElementById('requestMade');
+var reqDiv = document.getElementById('reqDiv');
 
+function changePic1(){
+    document.getElementById("circle1").src = "one.svg";
+}
 
+function changePic2(){
+    document.getElementById("circle2").src = "two.svg";
+}
 
+function sendToDB(){
+
+    // Set the "capital" field of the city 'DC'
+    db.collection('middle').doc('ground').update({
+        openRequests: true
+    })
+    .then(function() {
+        custConf.style.display = 'block';
+        security.style.display = 'none';
+
+        db.collection("middle").doc("ground")
+        .onSnapshot(function(doc) {
+        if ((doc.data().customerAccepted) == true) {
+            reqComplete.style.display = "block";
+            custConf.style.display = 'none';
+            security.style.display = 'none';
+        }
+    });
+    })
+    .catch(function(error) {
+        // The document probably doesn't exist.
+        console.error("Error updating document: ", error);
+    });
+}
+
+function backToMain(){
+    reqComplete.style.display = "none";
+    reqButton.style.display = "none";
+    blurBox.style.display = "block";
+    requestMade.style.display = "block";
+    reqDiv.style.display = "none";
+    document.getElementById('sadFace').style.display = "none";
+}
 
 function newRequest(){
     destination.style.display = 'block';
@@ -28,20 +69,16 @@ function securityLoad(){
 }
 
 function continuer(){
-    custConf.style.display = 'block';
+    compCreComplete.style.display = 'block';
     security.style.display = 'none';
 }
 
 function complete(){
-    custConf.style.display = 'none';
+    compCreComplete.style.display = 'none';
     reqButton.style.display = "none";
     blurBox.style.display = "block";
-
-}
-
-function doneFunc(){
-    compCreComplete.style.display = "none";
-    blurBox.style.display ="block";
+    requestMade.style.display = "block";
+    reqDiv.style.display = "none";
 
 }
 
--- web/Interior/OrgSystemPage/OrgSystemPage.js
+++ web/Interior/OrgSystemPage/OrgSystemPage.js
@@ -2,6 +2,7 @@ var blurBox = document.getElementById('blurBox');
 var newSystem = document.getElementById('newSystem');
 var compDetails = document.getElementById('compDetails');
 var compCreComplete = document.getElementById('compCreComplete');
+var right = document.getElementById("right");
 
 
 
@@ -27,7 +28,26 @@ function closer(){
     blurBox.style.display = 'block';
 }
 
+function doneFunc(){
+    compCreComplete.style.display = 'none';
+    blurBox.style.display = 'block';
+    document.getElementById('cusIntComp').style.display = "none";
+    document.getElementById('cusEntry').style.display = "block";
+    document.getElementById('accessDB').style.display = 'block';
+
+}
+
 function databaseRouter(){
     console.log("latch");
     window.location.href = '../OrgDatabase/OrgDatabase.html';
+}
+
+function openComp(){
+    document.getElementById('openComp').style.background = '#FBB70A';
+    document.getElementById('openComp').style.color = '#FFFFFF';
+}
+
+function textComp(){
+    document.getElementById('textInputButton').style.background = '#04D6B0';
+    document.getElementById('textInputButton').style.color = '#FFFFFF';
 }
